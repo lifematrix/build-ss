1db875c49a9b671f0140d2b8cc86e5aefdfb8be5
Steven Liu <stevenliucx@gmail.com>
2019-06-24 20:25:34 +0800

    init version

---
+++ build-ss.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apt update && apt upgrade -yuf
+apt install -y --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev \
+                                       asciidoc xmlto libev-dev libudns-dev automake libmbedtls-dev \
+                                       libsodium-dev git python-m2crypto libc-ares-dev
+
+cd /opt
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+
+cd shadowsocks-libev
+git submodule update --init --recursive
+
+./autogen.sh
+./configure
+make && make install
+
+adduser --system --no-create-home --group shadowsocks
+mkdir -m 755 /etc/shadowsocks
---
+++ config-ss.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# install rng-tools
+apt install rng-tools
+echo "HRNGDEVICE=/dev/urandom" >> /etc/default/rng-tools
+
+sudo adduser --system --no-create-home --group shadowsocks
+sudo cp local.conf /etc/sysctl.d/local.conf
+
+# create ss server
+sudo mkdir -p -m 755 /etc/shadowsocks
+sudo cp shadowsocks-template.json /etc/shadowsocks.json
+sudo sysctl --system
+
+# create systemd service
+sudo cp shadowsocks.service /etc/systemd/system/shadowsocks.service
+systemctl daemon-reload
+systemctl enable shadowsocks
+systemctl start shadowsocks
+
